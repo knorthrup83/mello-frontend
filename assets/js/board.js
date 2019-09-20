59ef39815c61f5dbb460fc5e49b92a8ca99d9898
knorthrup83@gmail.com <knorthrup83@gmail.com>
2019-09-20 17:55:38 +0000

    End of lesson 13

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,12 +1,17 @@
 class Card {
 	constructor(text) {
 		this.text = text;
+		this.id = ++Card.lastId;
 	}
 }
+
+Card.lastId = 0;
+
 
 class List {
 	constructor(title) {
 		this.title = title;
+		this.id = ++List.lastId;
 		this.cards = [];
 	}
 
@@ -14,10 +19,22 @@ class List {
 		var card = new Card(text);
 		this.cards.push(card);
 	}
+
+	findCard(cardId) {
+		return this.cards.find(function(card) {
+			if (cardId === card.id) {
+				return card;
+			}
+		});
+	}
 }
 
-class Board{
+List.lastId = 0;
+
+
+class Board {
 	constructor() {
+		this.id = ++Board.lastId;
 		this.lists = [];
 	}
 
@@ -25,7 +42,38 @@ class Board{
 		var list = new List(text);
 		this.lists.push(list);
 	}
+
+	findList(listId) {
+		return this.lists.find(function(list) {
+			return listId === list.id;
+		});
+	}
+
+	editList(listId, newTitle) {
+		var list = this.findList(listId);
+		if (list) {
+			list.title = newTitle;
+		}
+	}
+
+	addCard(listId, cardText) {
+		var list = this.findList(listId);
+		if (list) {
+			list.addCard(cardText);
+		}
+	}
+
+	editCard(cardId, cardText) {
+		this.lists.forEach(function(list) {
+			var card = list.findCard(cardId);
+			if (card) {
+				card.text = cardText;
+			}
+		});
+	}
 }
+
+Board.lastId = 0;
 
 var list = new List('My first list');
 list.addCard('My first card');
